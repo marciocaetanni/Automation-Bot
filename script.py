e3ad91ac90fac1cb0a085b36ee29eb74d76125ed
marciocaetanni <marciolorenzo1@hotmail.com>
2026-01-15 13:16:21 -0300

    Last commit after every test

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pyautogui
 
 # Setting up the global settings
-pyautogui.PAUSE = 0.5
+pyautogui.PAUSE = 1
 
 # Setting up the variables
 LINK = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
@@ -30,3 +30,39 @@ pyautogui.press("enter")
 table = pd.read_csv("products.csv")
 
 print(table)
+
+# Adding the products to the system
+for line in table.index:
+    pyautogui.click(x=456, y=256)
+
+    # código
+    CODIGO = str(table.loc[line, "codigo"])
+    pyautogui.write(CODIGO)
+    pyautogui.press("tab")
+    # marca
+    MARCA = str(table.loc[line, "marca"])
+    pyautogui.write(MARCA)
+    pyautogui.press("tab")
+    # tipo
+    TIPO = str(table.loc[line, "tipo"])
+    pyautogui.write(TIPO)
+    pyautogui.press("tab")
+    # categoria
+    CATEGORIA = str(table.loc[line, "categoria"])
+    pyautogui.write(CATEGORIA)
+    pyautogui.press("tab")
+    # preço
+    PRECO = str(table.loc[line, "preco_unitario"])
+    pyautogui.write(PRECO)
+    pyautogui.press("tab")
+    # custo
+    CUSTO = str(table.loc[line, "custo"])
+    pyautogui.write(CUSTO)
+    pyautogui.press("tab")
+    # obs
+    OBS = str(table.loc[line, "obs"])
+    pyautogui.write(OBS)
+    pyautogui.press("tab")
+
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
